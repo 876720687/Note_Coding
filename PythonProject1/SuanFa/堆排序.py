8283aa8985cf1c8d4a0157619f89225a5b3034fb
876720687 <876720687@qq.com>
2021-07-03 12:42:12 +0800

    suanfa

--- PythonProject1/SuanFa/堆排序.py
+++ PythonProject1/SuanFa/堆排序.py
@@ -20,11 +20,25 @@ def sift(li ,low ,high): # 实现了建堆的过程
     else:
         li[i] = tmp
 
+# def heap_sort(li):
+#     n = len(len)
+#     for i in range((n-2)//2, -1, -1):
+#         sift(li, i, n-1)
+#     for i in range(n-1, -1 -1):
+#         li[0], li[i] = li[i], li[0]
+#         sift(li, 0, i-1)
+
 def heap_sort(li):
-    n = len(len)
+    n = len(li)
     for i in range((n-2)//2, -1, -1):
-        sift(li, i, n-1)
-    for i in range(n-1, -1 -1):
-        li[0], li[i] = li[i], li[0]
-        sift(li, 0, i-1)
+        sift(li, i ,n-1)
+    # 建堆完成
+    print(li)
+
+li = [i for i in range(100)]
+import random
+random.shuffle(li)
+
+heap_sort(li)
+
 
